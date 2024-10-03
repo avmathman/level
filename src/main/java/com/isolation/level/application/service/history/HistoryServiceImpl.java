b6a9c363f5d5d34c9e5aafc718d196b6b6cf0935
Abdurashid Vakhobov <avcoding01@gmail.com>
2024-10-04 00:30:40 +0500

    Implements logic for different isolation levels

--- src/main/java/com/isolation/level/application/service/history/HistoryServiceImpl.java
+++ src/main/java/com/isolation/level/application/service/history/HistoryServiceImpl.java
@@ -2,43 +2,112 @@ package com.isolation.level.application.service.history;
 
 import com.isolation.level.application.mapper.HistoryMapper;
 import com.isolation.level.application.model.HistoryModel;
+import com.isolation.level.domain.book.BookEntity;
+import com.isolation.level.domain.history.HistoryEntity;
+import com.isolation.level.infrastructure.repository.BookRepository;
 import com.isolation.level.infrastructure.repository.HistoryRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
+@Slf4j
 @Service
 public class HistoryServiceImpl implements HistoryService {
 
     private final HistoryRepository repository;
     private final HistoryMapper mapper;
+    private final BookRepository bookRepository;
 
     public HistoryServiceImpl(
             HistoryRepository repository,
-            HistoryMapper mapper
+            HistoryMapper mapper,
+            BookRepository bookRepository
     ) {
         this.repository = repository;
         this.mapper = mapper;
+        this.bookRepository = bookRepository;
     }
 
     @Override
     @Transactional(isolation = Isolation.READ_COMMITTED)
     public HistoryModel createHistoryReadCommitted(HistoryModel historyModel) {
-        return mapper.entityToModel(repository.save(mapper.modelToEntity(historyModel)));
+        HistoryEntity historyEntity = mapper.modelToEntity(historyModel);
+        HistoryEntity createdHistoryEntity = null;
+
+        BookEntity book = bookRepository.findByTitle(historyModel.getTitle()).orElse(null);
+
+        if (book == null) {
+            historyEntity.setStatus("RECEIVED");
+            createdHistoryEntity = repository.save(historyEntity);
+
+            book.setLikes(book.getLikes() + historyModel.getLikes());
+            bookRepository.save(book);
+
+            log.info("{} likes added to {}", historyModel.getLikes(), book.getAuthor());
+        } else {
+            log.warn("Book with title {} not found", historyModel.getTitle());
+
+            historyEntity.setStatus("ORPHANED");
+            createdHistoryEntity = repository.save(historyEntity);
+        }
+
+        return mapper.entityToModel(repository.save(createdHistoryEntity));
     }
 
     @Override
     @Transactional(isolation = Isolation.REPEATABLE_READ)
     public HistoryModel createHistoryRepeatableRead(HistoryModel historyModel) {
-        return mapper.entityToModel(repository.save(mapper.modelToEntity(historyModel)));
+        HistoryEntity historyEntity = mapper.modelToEntity(historyModel);
+        HistoryEntity createdHistoryEntity = null;
+
+        BookEntity book = bookRepository.findByTitle(historyModel.getTitle()).orElse(null);
+
+        if (book == null) {
+            historyEntity.setStatus("RECEIVED");
+            createdHistoryEntity = repository.save(historyEntity);
+
+            book.setLikes(book.getLikes() + historyModel.getLikes());
+            bookRepository.save(book);
+
+            log.info("{} likes added to {}", historyModel.getLikes(), book.getAuthor());
+        } else {
+            log.warn("Book with title {} not found", historyModel.getTitle());
+
+            historyEntity.setStatus("ORPHANED");
+            createdHistoryEntity = repository.save(historyEntity);
+        }
+
+        return mapper.entityToModel(repository.save(createdHistoryEntity));
     }
 
     @Override
     @Transactional(isolation = Isolation.SERIALIZABLE)
     public HistoryModel createHistorySerializable(HistoryModel historyModel) {
-        return mapper.entityToModel(repository.save(mapper.modelToEntity(historyModel)));
+        HistoryEntity historyEntity = mapper.modelToEntity(historyModel);
+        HistoryEntity createdHistoryEntity = null;
+
+        BookEntity book = bookRepository.findByTitle(historyModel.getTitle()).orElse(null);
+
+        if (book == null) {
+            historyEntity.setStatus("RECEIVED");
+            createdHistoryEntity = repository.save(historyEntity);
+
+            book.setLikes(book.getLikes() + historyModel.getLikes());
+            bookRepository.save(book);
+
+            log.info("{} likes added to {}", historyModel.getLikes(), book.getAuthor());
+        } else {
+            log.warn("Book with title {} not found", historyModel.getTitle());
+
+            historyEntity.setStatus("ORPHANED");
+            createdHistoryEntity = repository.save(historyEntity);
+        }
+
+        return mapper.entityToModel(repository.save(createdHistoryEntity));
     }
 
     @Override
--- src/main/java/com/isolation/level/infrastructure/repository/BookRepository.java
+++ src/main/java/com/isolation/level/infrastructure/repository/BookRepository.java
@@ -4,9 +4,12 @@ import com.isolation.level.domain.book.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * Data repository for books.
  */
 @Repository
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
+    Optional<BookEntity> findByTitle(String author);
 }
